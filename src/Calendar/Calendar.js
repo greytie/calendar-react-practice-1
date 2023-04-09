3e9256180825f8ef65cab17eaf2483056699fdc4
UltraFutile <ultrafutile@protonmail.com>
2023-04-09 17:29:27 -0500

    Add month name header

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -14,17 +14,22 @@ export const Calendar = () => {
     const appendedCells = Array.from({length: 5}, (x, i) => i + 1)
 
     return (
-        <div className="month-container Rtable Rtable--7cols">
-            {
-                daysOfWeek.map((x, idx) => <div key={idx} className="Rtable-cell">{x}</div>)
-            }
-            {
-                days.map((x, idx) => <DayCell key={idx + 7} day={x}></DayCell>)
-            }
-            {
-                appendedCells.map((x, idx) => <div key={idx + 37} className="Rtable-cell day-cell"></div>)
-            }
-        </div>
+        <>
+            <div className="month-header">
+                dec
+            </div>
+            <div className="month-container Rtable Rtable--7cols">
+                {
+                    daysOfWeek.map((x, idx) => <div key={idx} className="day-of-week Rtable-cell">{x}</div>)
+                }
+                {
+                    days.map((x, idx) => <DayCell key={idx + 7} day={x}></DayCell>)
+                }
+                {
+                    appendedCells.map((x, idx) => <div key={idx + 37} className="Rtable-cell day-cell"></div>)
+                }
+            </div>
+        </>
     );
 };
 
